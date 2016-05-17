03fc01270f574dbcca2f64d856867bb48654cbc7
OHINTAEK <flytaek@gmail.com>
2016-05-17 17:26:23 +0900

    Reist

---
+++ LSAdapter/src/sine/linkstar/adapter/test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package sine.linkstar.adapter;
+
+/**
+ * @author dev1046
+ *
+ */
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
